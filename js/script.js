ae89726ed33fc23b7b9075af6451b76b15973445
Amir01020 <amirrodjabov@gmail.com>
2023-06-19 14:03:08 +0500

    next22

--- js/script.js
+++ js/script.js
@@ -1,46 +1,66 @@
 let d = document
-var swiper = new Swiper(".mySwiper", {
-  direction: "vertical",
-  slidesPerView: 1,
-  spaceBetween: 30,
-  mousewheel: true,
-  speed: 800,
+var swiper2 = new Swiper(".mySwiper", {
+  grabCursor: true,
   effect: "fade",
-  pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-  },  
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-});
-
-var swiper = new Swiper(".mySwiper1", {
-  slidesPerView: 2,
-  spaceBetween: 20,
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: ["-120%", 0, -500],
+    },
+    next: {
+      shadow: true,
+      translate: ["120%", 0, -500],
+    },
+  },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    type: "fraction",
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
-let z =1
 
-VANTA.WAVES({
-  el: "#fonss",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: window.innerHeight,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  color: '#012413',
-  shininess: 40.00,
-  waveHeight: 40,
-  zoom: 0.6
-})
+
+let fonsPar = d.querySelectorAll('#fonsPar')
+fonsPar.forEach(i => {
+  VANTA.WAVES({
+    el: i,
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: window.innerHeight,
+    minWidth: window.innerWidth,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: i.dataset.color,
+    shininess: 40.00,
+    waveHeight: 40,
+    zoom: 0.7
+  })
+});
+let arr = [
+  {
+    img: '',
+    name: '',
+    comment: '',
+    color: '',
+    id: ''
+  }
+]
+let obj = {
+  name: '',
+  comment: '',
+  id: ""
+}
+let names = d.querySelector('#name')
+let comment = d.querySelector('#comment')
+let post = () => {
+  obj.name = names.value
+  obj.comment = comment.value
+  axios.post('https://tobacco-b5bb1-default-rtdb.firebaseio.com/items.json', obj)
+
+}
+let btn = d.querySelector('#btn')
+btn.addEventListener('click', post())
